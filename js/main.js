ffedc5967399701d19744962a3cdf249a0787f9d
aakbar2022 <aakbar@zstronics.com>
2023-05-02 20:10:18 +0500

    updated crausal

--- js/main.js
+++ js/main.js
@@ -75,3 +75,21 @@ $('.accordion-header').click(function(){
 });
 
 });
+
+$('#carouselControls').on('slide.bs.carousel', function (e) {
+  let $e = $(e.relatedTarget),
+        itemsPerSlide = 3,
+        totalItems = $('.carousel-item', this).length,
+        $itemsContainer = $('.carousel-inner', this),
+        it = itemsPerSlide - (totalItems - $e.index());
+    if (it > 0) {
+      for (var i = 0; i < it; i++) {
+        $('.carousel-item', this).eq(e.direction == "left" ? i : 0).
+          // append slides to the end/beginning
+          appendTo($itemsContainer);
+      }
+    }
+  });
+  $('.carousel').carousel({
+    interval: 10000
+  })
